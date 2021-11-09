e7ea83203472ed0cb07afe048db1faf8c34df277
unknown <sourabahmed>
2021-11-10 00:13:39 +0600

    navigation-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
     </div>
   );
 }
---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h2>This is home</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
