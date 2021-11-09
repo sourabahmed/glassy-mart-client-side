fd0dd3a2b2cb15ce9c3be84cf3b8e1b64f8fd75f
unknown <sourabahmed>
2021-11-10 05:21:50 +0600

    first-day-done

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Service.css'
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { productName, url, price , description} = service;
     return (
-        <div>
-            
+        <div className="service">
+            <div>
+                <img className="img-fluid" src={url} alt="" />
+            </div>
+            <div className="p-3">
+                <h5>{productName}</h5>
+                <h4>{price}</h4>
+                <button className="btn btn-danger">Purchase</button>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
     console.log(services);
     useEffect(() => {
         fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            {   
-                services.map(service => <Service
-                service={service}
-                key={service._id}
-                ></Service>)
-            }
+            <div className="services">
+                {
+                    services.map(service => <Service
+                        service={service}
+                        key={service._id}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
